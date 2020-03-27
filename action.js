c736f41c9bd45c626b84129c25ecc3689a9a5186
Tomasz Turek <ttomaszito@gmail.com>
2020-03-27 17:10:23 -0400

    added function to calculate totals, altered formats and added propper date formats

--- action.js
+++ action.js
@@ -16,30 +16,30 @@ fs.createReadStream("./Data/raw/Cases_raw.csv")
       let country = element["Country/Region"].toUpperCase();
 
       formated_data[country] = {
-        infected_total: 0,
-        deaths_total: 0,
-        recovered_total: 0,
-        time_series: { infected: null }
+        TOTAL_INFECTED: 0,
+        TOTAL_DEATHS: 0,
+        TOTAL_RECOVERED: 0,
+        TIME_DATA: { INFECTED: null }
       };
     });
-    formated_data = formatData(INFECTED_DATA, formated_data, "infected");
+    formated_data = formatData(INFECTED_DATA, formated_data, "INFECTED");
   });
 // Read and merge Death data with new format
 fs.createReadStream("./Data/raw/Death_raw.csv")
   .pipe(csv("Province/States", "Country/Region"))
   .on("data", data => DEATH_DATA.push(data))
   .on("end", () => {
-    formated_data = formatData(DEATH_DATA, formated_data, "deaths");
+    formated_data = formatData(DEATH_DATA, formated_data, "DEATHS");
   });
 // Read and merge Recovered data with new format
 fs.createReadStream("./Data/raw/Recovered_raw.csv")
   .pipe(csv("Province/States", "Country/Region"))
   .on("data", data => RECOVERED_DATA.push(data))
   .on("end", () => {
-    formated_data = formatData(RECOVERED_DATA, formated_data, "recovered");
+    formated_data = formatData(RECOVERED_DATA, formated_data, "RECOVERED");
 
-    //formated_data = applyStatistics(formated_data);
-
+    formated_data = getTotals(formated_data);
+    console.log(formated_data);
     saveToFile(formated_data);
   });
 
@@ -52,11 +52,22 @@ saveToFile = data => {
     }
   );
 };
+/**
+ * Modifies the JSON data to include totals for each country
+ * Deaths,Infected and Recoveries
+ * @param  JSON format data
+ */
+getTotals = data => {
+  Object.entries(data).forEach(([country, stats]) => {
+    let infected = Math.max(...Object.values(stats["TIME_DATA"]["INFECTED"]));
+    let deaths = Math.max(...Object.values(stats["TIME_DATA"]["DEATHS"]));
+    let recovered = Math.max(...Object.values(stats["TIME_DATA"]["RECOVERED"]));
 
-applyStatistics = data => {
-  Object.entries(data).forEach(([key, value]) => {
-    console.log(value);
+    stats["TOTAL_INFECTED"] = infected;
+    stats["TOTAL_DEATHS"] = deaths;
+    stats["TOTAL_RECOVERED"] = recovered;
   });
+  return data;
 };
 
 formatData = (INFECTED_DATA, formated_data, type) => {
@@ -64,18 +75,19 @@ formatData = (INFECTED_DATA, formated_data, type) => {
   INFECTED_DATA.forEach(element => {
     let country = element["Country/Region"].toUpperCase();
     //console.log(country)
-    let timedata = formated_data[country]["time_series"];
+    let timedata = formated_data[country]["TIME_DATA"];
 
     Object.entries(element).forEach(([key, value]) => {
       let dateReg = /^[0-9]{1,2}[/][0-9]{2}[/][0-9]{2}$/g;
 
       if (dateReg.test(key)) {
+        let date = new Date(key).toJSON().split("T")[0];
         if (timedata[[type]] == null) {
-          timedata[[type]] = { [key]: parseInt(value) };
-        } else if (timedata[[type]][key] == null) {
-          timedata[[type]][key] = parseInt(value);
+          timedata[[type]] = { [date]: parseInt(value) };
+        } else if (timedata[[type]][date] == null) {
+          timedata[[type]][date] = parseInt(value);
         } else {
-          timedata[[type]][key] += parseInt(value);
+          timedata[[type]][date] += parseInt(value);
         }
       }
     });
