b2927d0090d2d948a34914fbdf4ba75ce189b995
Tomasz Turek <ttomaszito@gmail.com>
2020-04-14 14:30:14 -0400

    added archiving of daily usa data

--- save.js
+++ save.js
@@ -5,19 +5,23 @@ readFileData = (source) => {
   let json = JSON.parse(buffer);
   return json;
 };
-writeFileData = (data, date) => {
+writeFileData = (data, date, type) => {
   jsonString = JSON.stringify(data);
-  fs.writeFileSync(`./Data/archive/${date}-global.json`, jsonString);
+  fs.writeFileSync(`./Data/archive/${date}-${type}.json`, jsonString);
 };
 
 // Save Daily Global Statistics
 daily_global_output = {};
+daily_Usa_data = {};
 
 let global_data = readFileData("./Data/data_sets/global_data_v2.json");
+let usa_data = readFileData("./Data/data_sets/usa_data_v2.json");
+
 let date = new Date();
 date.setDate(date.getDate() - 1);
 date = date.toJSON().split("T")[0];
 
-writeFileData(global_data[date], date);
+writeFileData(global_data[date], date, "global");
+writeFileData(usa_data[date], date, "usa");
 
 console.log("Save complete");
